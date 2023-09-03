6a1f3c35fd30ee21bc7ff7de259f87b738f58ba0
tanvir5230 <tanvirbintouhid5230@gmail.com>
2023-09-03 17:08:52 +0600

    Broke down the structure into atoms, molecules etc. and create file and folder for each part. Just the file names, not code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="lg:flex" style={{ height: "100vh" }}>
+      <div className="lg:w-2/5 md:w-1/2 sm:w-full bg-red-400 h-full">
+        <p>Left div</p>
+      </div>
+      <div className="lg:w-3/5 md:w-1/2 sm:w-ful bg-green-300">
+        <p>Right Div</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
