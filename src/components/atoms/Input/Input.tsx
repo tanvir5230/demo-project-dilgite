54b8cd832b9a8b4fc86a6a903bb1240ae510e638
tanvir5230 <tanvirbintouhid5230@gmail.com>
2023-09-04 12:06:32 +0600

    Input atom created

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface InputInterface {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  placeholder?: string;
+  id?: "";
+  name?: "";
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const Input: React.FC<InputInterface> = ({
+  value,
+  setValue,
+  id = "",
+  name = "",
+  placeholder,
+  style,
+  className,
+}) => {
+  return (
+    <input
+      type="text"
+      value={value}
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      style={style ? style : {}}
+      className={className ? className : ""}
+      onChange={e => setValue(e.target.value)}
+    />
+  );
+};
+
+export default Input;
