21db5077ac13053838425edb01c8c4d3c15a1e60
tanvir5230 <tanvirbintouhid5230@gmail.com>
2023-09-04 10:20:23 +0600

    Horizontal Line is created as an atom

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="lg:flex" style={{ height: "100vh" }}>
-      <div className="lg:w-2/5 md:w-1/2 sm:w-full bg-red-400 h-full">
-        <p>Left div</p>
-      </div>
-      <div className="lg:w-3/5 md:w-1/2 sm:w-ful bg-green-300">
+    <div
+      className="lg:flex justify-center"
+      style={{ height: "100vh", alignItems: "center" }}
+    >
+      <div
+        className="lg:w-2/5 md:w-1/2 sm:w-full bg-red-400 rounded-md"
+        style={{ width: "400px", height: "800px" }}
+      ></div>
+      <div
+        className="lg:w-3/5 md:w-1/2 sm:w-ful bg-green-300 rounded-md"
+        style={{ width: "800px", height: "800px" }}
+      >
         <p>Right Div</p>
       </div>
     </div>
---
+++ src/components/atoms/HRLine/HRLine.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HRLine: React.FC = () => {
+  return <hr className="w-64 h-1 bg-blue-50 border-0 mx-auto my-8" />;
+};
+
+export default HRLine;
