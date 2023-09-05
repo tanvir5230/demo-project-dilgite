447d15ca1c113f54a44d2fe5c88d1eaa5be1fb27
tanvir5230 <tanvirbintouhid5230@gmail.com>
2023-09-05 14:11:49 +0600

    Zustand and tanstack/react-query installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./index.css";
 import Homepage from "./components/pages/Homepage/Homepage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App: React.FC = () => {
-  return <Homepage />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Homepage />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
